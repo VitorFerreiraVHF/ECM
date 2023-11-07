59ed21dd068004cdc5c42d3c8cde4fb7143a9952
vitor.borges@bit.com.br <vitor.borges@DEVSECOPS03>
2023-11-07 20:16:42 -0300

    pastas

---
+++ desenvolvimento/Api/ECM.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using ECM.Dominio.Interfaces.Repositorios;
+using ECM.Dominio.Interfaces.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECM.WebApi.Controllers
+{
+
+    [ApiController]
+    public class UsersController : Controller
+    {
+        //private readonly IUserService _userService;
+        //public UsersController(IUserService userService)
+        //{
+        //    _userService = userService;
+        //}
+        [HttpGet]
+        [Route("/cadastro")]
+        public IActionResult Cadastro()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        [Route("/login")]
+        public IActionResult login( string email, string senha)
+        {
+            return Redirect("");
+        }
+    }
+}
+
---
+++ desenvolvimento/Api/ECM.WebApi/Program.cs
@@ -0,0 +1,40 @@
+using System.Transactions;
+using ECM.Aplicacao.Servicos;
+using ECM.Dominio.Interfaces.Repositorios;
+using ECM.Dominio.Interfaces.Servicos;
+using ECM.infraestrutura.Dados.Repositorios;
+using ECM.WebApi.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddAutoMapper(typeof(DomainToPresentention),typeof(PresententionToDomain));
+//builder.Services.AddAutoMapper(typeof(PresententionToDomain),typeof(DomainToPresentention));
+
+//builder.Services.AddScoped<IUserService, UserService>();
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
